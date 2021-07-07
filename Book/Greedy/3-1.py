f30c923c03d65de11e03becbbbae395d14d9356b
salixkang <salixkang@gmail.com>
2021-07-07 19:38:17 +0900

    update 3-1

--- Book/Greedy/3-1.py
+++ Book/Greedy/3-1.py
@@ -1,2 +1,28 @@
 # Minimum number of coins to exchange N with 500, 100, 50, 10
 
+N = 10023450
+
+coin_num = 0
+
+coin_num = N // 500
+
+N = N % 500
+
+coin_num += N // 100
+
+N = N % 100
+
+coin_num += N // 50
+
+N = N % 50
+
+coin_num += N // 10
+
+print(coin_num)
+
+
+
+
+
+
+
