a8dfb8dd14b5490ca467a140edb331d3a7f5d7a4
salixkang <salixkang@gmail.com>
2021-08-27 18:37:33 +0900

    update 6-6 ~ 6-10

---
+++ Book/Python/resource.py
@@ -0,0 +1,27 @@
+# single input
+# n = int(input())
+#
+# multi-variable input
+# n, m = map(int, input().split())
+#
+# graph input
+# graph = [list(map(int, input())) for _ in range(n)]
+# or
+# for i in range(n):
+#     graph[i] = list(map(int, input().split()))
+#
+# array input
+# array = []
+# for i in range(n):
+#     array.append(int(input()))
+#
+# sort library
+# array = sorted(array)
+# array = sorted(array, reverse = True)
+# array.sort()
+# array.sort(reverse = True)
+#
+# if statement
+# if x = 0 :
+# elif x = 1:
+# else :
---
+++ Book/Sorting/6-10.py
@@ -0,0 +1,9 @@
+# Descending Algorithm
+
+n = int(input())
+array = []
+for i in range(n):
+    array.append(int(input()))
+
+array.sort(reverse=True)
+print(array)
