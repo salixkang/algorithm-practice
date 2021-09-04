78f7afa63248cb253ef8e661eed40e136e9ee792
salixkang <salixkang@gmail.com>
2021-09-04 22:38:22 +0900

    update 8-7

---
+++ Book/Dynamic Programming/8-8.py
@@ -0,0 +1,17 @@
+# monetary composition
+
+n, m = map(int, input().split())
+
+d = [0] * 10001
+
+money = []
+for _ in range(n):
+    money.append(int(input()))
+
+money.sort(reverse=True)
+
+for i in range(1, n+1):
+    if i % money[0] == 0:
+        d[i] = i//money[0]
+    else:
+        
