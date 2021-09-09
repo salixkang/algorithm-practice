5a7694c6e60df6e21c16a7398a313bb6abf6a3e8
salixkang <salixkang@gmail.com>
2021-09-09 19:46:43 +0900

    update 10-3 ~ 10-4

--- Book/Graph Algorithm/10-4.py
+++ Book/Graph Algorithm/10-4.py
@@ -1,9 +1,10 @@
-# developed disjoint set algorithm
+# cycle
 
 def find_parent(parent, x):
     if parent[x] != x:
         parent[x] = find_parent(parent, parent[x])
     return parent[x]
+
 
 def union_parent(parent, a, b):
     a = find_parent(parent, a)
@@ -13,6 +14,7 @@ def union_parent(parent, a, b):
     else:
         parent[a] = b
 
+
 v, e = map(int, input().split())
 
 parent = [0] * (v + 1)
@@ -20,16 +22,18 @@ parent = [0] * (v + 1)
 for i in range(1, v + 1):
     parent[i] = i
 
+cycle = False
+
 for i in range(e):
     a, b = map(int, input().split())
-    union_parent(parent, a, b)
+    if find_parent(parent, a) == find_parent(parent, b):
+        cycle = True
+        break
+    else :
+        union_parent(parent, a, b)
 
-print('각 원소가 속한 집합 : ', end = '')
-for i in range(1, v + 1):
-    print(find_parent(parent, i), end = ' ')
+if cycle:
+    print("사이클이 발생했습니다.")
+else:
+    print("사이클이 발생하지 않았습니다.")
 
-print()
-
-print('부모 테이블 : ', end = '')
-for i in range(1, v + 1):
-    print(parent[i], end = ' ')
