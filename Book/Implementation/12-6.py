f28bd79fd6c8dde27e88f12e684ca116d729f439
salixkang <salixkang@gmail.com>
2021-09-26 21:44:59 +0900

    update 12-6

--- Book/Implementation/12-6.py
+++ Book/Implementation/12-6.py
@@ -2,55 +2,37 @@
 # https://programmers.co.kr/learn/courses/30/lessons/60061
 
 def solution(n, build_frame):
-    from operator import itemgetter, attrgetter
-
     answer = []
 
-    column = []
-    girder = []
-    girder_end = []
-    column_end = []
-
     for i in build_frame:
-        if i[2] == 0:
-            if i[3] == 1:
-                if i[1] == 0 or (i[0], i[1]) in column_end or (i[0], i[1]) in girder or (i[0], i[1]) in girder_end:
-                    column.append((i[0], i[1]))
-                    column_end.append((i[0], i[1] + 1))
-            elif i[3] == 0:
-                if ((i[0], i[1] + 1) in column and (i[0], i[1] + 1) not in girder and (
-                i[0], i[1] + 1) not in girder_end) or ((i[0], i[1] + 1) in girder and (
-                        ((i[0], i[1] + 1) not in girder_end or (i[0] + 1, i[1] + 1) not in girder) or (
-                i[0] + 1, i[1] + 1) not in column_end)) or ((i[0], i[1] + 1) in girder_end and (
-                        ((i[0] - 1, i[1] + 1) not in girder_end or (i[0], i[1] + 1) not in girder) or (
-                i[0] - 1, i[1] + 1) not in column_end)):
-                    continue
-                else:
-                    column.remove((i[0], i[1]))
-                    column_end.remove((i[0], i[1] + 1))
+        x, y, stuff, opr = i[0], i[1], i[2], i[3]
+        if opr == 1:
+            answer.append([x, y, stuff])
+            if possible(answer) == False:
+                answer.remove([x, y, stuff])
+        elif opr == 0:
+            answer.remove([x, y, stuff])
+            if possible(answer) == False:
+                answer.append([x, y, stuff])
 
-        elif i[2] == 1:
-            if i[3] == 1:
-                if (i[0], i[1]) in column_end or (i[0] + 1, i[1]) in column_end or (
-                        (i[0], i[1]) in girder_end and (i[0] + 1, i[1]) in girder):
-                    girder.append((i[0], i[1]))
-                    girder_end.append((i[0] + 1, i[1]))
-
-            elif i[3] == 0:
-                if (i[0], i[1]) in column and ((i[0], i[1]) not in column_end and (i[0], i[1]) not in girder_end) or (
-                        (i[0], i[1]) in girder_end and (i[0], i[1]) not in column_end and (
-                i[0] - 1, i[1]) not in column_end) or (
-                        (i[0] + 1, i[1]) in girder and (i[0] + 1, i[1]) not in column_end and (
-                i[0] + 2, i[1]) not in column_end):
-                    continue
-                girder.remove((i[0], i[1]))
-                girder_end.remove((i[0] + 1, i[1]))
-
-    for i in column:
-        answer.append([i[0], i[1], 0])
-
-    for i in girder:
-        answer.append([i[0], i[1], 1])
-
-    answer = sorted(answer, key=itemgetter(0, 1, 2))
+    answer = sorted(answer, key=lambda x: (x[0], x[1], x[2]))
     return answer
+
+
+def possible(answer):
+    for i in answer:
+        x, y, stuff = i[0], i[1], i[2]
+        if stuff == 0:
+            if y == 0 or ([x, y, 1] in answer) or ([x - 1, y, 1] in answer) or ([x, y - 1, 0] in answer):
+                continue
+            else:
+                return False
+        elif stuff == 1:
+            if ([x, y - 1, 0] in answer) or ([x + 1, y - 1, 0] in answer) or (
+                    ([x - 1, y, 1] in answer) and ([x + 1, y, 1] in answer)):
+                continue
+            else:
+                return False
+        return True
+
+print(solution(5, [[1, 0, 0, 1], [1, 1, 1, 1], [2, 1, 0, 1], [2, 2, 1, 1], [5, 0,0, 1], [5, 1, 0, 1], [4, 2, 1, 1], [3, 2, 1, 1]]))
