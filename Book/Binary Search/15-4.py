06ee0841442a153e28c0e742a732857499f7303b
salixkang <salixkang@gmail.com>
2021-11-01 00:51:20 +0900

    update 15-4

--- Book/Binary Search/15-4.py
+++ Book/Binary Search/15-4.py
@@ -11,36 +11,68 @@ def solution(words, queries):
     for query in queries:
         if query[0] == '?':
             res = back_query(query, words)
+            answer.append(res)
         else:
             res = front_query(query, words)
-            print(res)
+            answer.append(res)
 
     return answer
 
 
 def front_query(query, words):
-    temp = []
-    for i in query:
-        if i != '?':
-            temp.append(i)
-        else:
-            break
+    temp = list(filter(lambda val: val != '?', query))
+    print(temp)
+    # for i in query:
+    #     if i != '?':
+    #         temp.append(i)
+    #     else:
+    #         break
 
     start = "".join(temp)
     temp[-1] = chr(ord(temp[-1]) + 1)
     end = "".join(temp)
 
     inter = words[bisect_left(words, start):bisect_left(words, end)]
-    inter.sort(key=len)
-    res = 0
-    for i in inter:
-        if len(i) == len(query):
-            res += 1
-        elif len(i) >= len(query):
-            break
+    res = length(query, inter)
 
     return res
 
 
 def back_query(query, words):
+    temp = list(filter(lambda val: val != '?', query))
+    if not temp:
+        return length(query, words)
+    len_q = len(words) - len(temp)
+
     return 0
+
+
+def length(query, words):
+    words.sort(key=len)
+    len_query = len(query)
+    start = 0
+    end = len(words)
+    left = start
+    right = end
+    while start < end:
+        mid = (start + end) // 2
+        if len(words[mid]) < len_query:
+            start = mid + 1
+        else:
+            end = mid
+
+    left = start
+
+    start = 0
+    end = len(words)
+
+    while start < end:
+        mid = (start + end) // 2
+        if len(words[mid]) > len_query:
+            end = mid
+        else:
+            start = mid + 1
+
+    right = start
+
+    return len(words[left:right])
