b40f8d718e7d9b7ef63624f496ab6b8a0baf5b91
salixkang <salixkang@gmail.com>
2021-11-05 00:31:25 +0900

    update 16-3, 16-4

--- Book/Dynamic Programming/16-3.py
+++ Book/Dynamic Programming/16-3.py
@@ -1,3 +1,28 @@
 # leave
 # https://www.acmicpc.net/problem/14501
 
+n = int(input())
+plan = [[] for _ in range(n)]
+for i in range(n):
+    t, p = map(int, input().split())
+    plan[i] = (t, p)
+
+# dp[k] = max(plan[k][1] + dp[k + plan[k][0]], dp[k + 1])
+
+dp = [0] * n
+
+for k in range(n-1, -1, -1):
+    if (k + plan[k][0]) > n:
+        if k == n-1:
+            dp[k] = 0
+        else:
+            dp[k] = dp[k + 1]
+    elif (k + plan[k][0]) == n :
+        if k == n-1:
+            dp[k] =plan[k][1]
+        else:
+            dp[k] = max(plan[k][1], dp[k + 1])
+    else:
+        dp[k] = max(plan[k][1] + dp[k + plan[k][0]], dp[k + 1])
+
+print(dp[0])
---
+++ Book/Dynamic Programming/16-4.py
@@ -0,0 +1,7 @@
+# soldier
+# https://www.acmicpc.net/problem/18353
+
+n = int(input())
+power = list(map(int, input().split()))
+
+#
